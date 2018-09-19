ec6c59e70db17ad1cdaae724d1a1c8eebe5c3b13
github username <github email address>
2018-09-19 09:46:03 +0100

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(name)    #writes name of dog to an instance variable
+    @name = name
+  end
+
+  def name   #reads name of dog from instance variable
+    @name
+  end
+
+  def breed=(breed)   #writes breed of dog to instance variable
+    @breed = breed
+  end
+
+  def breed     #reads breed of dog from instance variable
+    @breed
+  end
+
+end
